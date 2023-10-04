67db134a130f6fff21a6f111259addff208011df
Durgesh <shukladurgesh64@gmail.com>
2023-10-04 18:26:39 +0530

    Deployed website

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.jpg';
+import product7 from './assets/7.jpg';
+import product8 from './assets/8.jpg';
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: 'IPhone',
+    price: 155556,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: 'Macbook',
+    price: 200001,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: 'Camera',
+    price: 80001,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: 'Jacket',
+    price: 1556,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: 'LED Strip Lights',
+    price: 556,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: 'T-shirt',
+    price: 1555,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: 'Cap',
+    price: 155,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: 'Shirt',
+    price: 1896,
+    productImage: product8,
+  },
+];
